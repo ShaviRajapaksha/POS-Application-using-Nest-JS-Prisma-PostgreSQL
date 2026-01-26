14f9eda956d63c43a6ca3edf397da95094d355c0
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-27 01:34:49 +0530

    refactor: format CreateProductDto for improved readability

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,24 +1,33 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
 import { IsNotEmpty, IsNumber, IsOptional, IsString, Min } from "class-validator";
 
 export class CreateProductDto {
-    @ApiProperty({example: 'Laptop'})
-    @IsString()
-    @IsNotEmpty()
-    name: string;
 
-    @ApiProperty({example: 'High-performance laptop', required: false})
-    @IsString()
-    @IsOptional()
-    description?: string;
+  @ApiProperty({ example: 'Laptop' })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-    @ApiProperty({example: 50})
-    @IsNumber()
-    @Min(0)
-    stock: number;
+  @ApiProperty({ example: 'High-performance laptop', required: false })
+  @IsOptional()
+  @IsString()
+  description?: string;
 
-    @ApiProperty({example: 'Lap-001'})
-    @IsString()
-    @IsNotEmpty()
-    sku: string;
+  @ApiProperty({ example: 50 })
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
+  stock: number;
+
+  @ApiProperty({ example: 'Lap-001' })
+  @IsString()
+  @IsNotEmpty()
+  sku: string;
+
+  @ApiProperty({ example: 123.45 })
+  @Type(() => Number)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  price: number;
 }
