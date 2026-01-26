1c74b4a95b2a2d99adedfa44d9c236ecaac8691f
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-26 13:17:02 +0530

    feat: enhance main.ts with global validation and Swagger setup

--- pos-backend/src/main.ts
+++ pos-backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('POS Backend')
+    .setVersion('1.0')
+    .build();
+
+    SwaggerModule.setup(
+      'api',
+      app,
+      SwaggerModule.createDocument(app, config),
+    );
+    await app.listen(3000);
 }
 bootstrap();
