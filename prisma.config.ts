f3ca9394190b9b09f78c41c2084c8249fb55dbf6
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-28 21:46:38 +0530

    refactor: reorganize prisma.config.ts for clarity and structure

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,14 @@
 // This is not neccessory for the project
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 export default defineConfig({
   schema: "prisma/schema.prisma",
+    datasource: {
+    url: process.env.DATABASE_URL!,
+  },
   migrations: {
     path: "prisma/migrations",
   },
