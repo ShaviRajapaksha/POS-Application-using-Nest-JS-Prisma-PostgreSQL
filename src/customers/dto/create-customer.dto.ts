d7df1d5e2cdb1718b015c370a7d5e417023d9c3d
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-28 23:45:38 +0530

    feat: create CreateCustomerDto with validation and API documentation

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,24 @@
-export class CreateCustomerDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateCustomerDto {
+    @ApiProperty({ example: 'Sam Smith' })
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty({ example: 'samsmith@example.com', required: false })
+    @IsEmail()
+    @IsOptional()
+    email?: string;
+
+    @ApiProperty({ example: '+94771234567', required: false })
+    @IsString()
+    @IsOptional()
+    phone?: string;
+
+    @ApiProperty({ example: 'Main st, Colombo', required: false })
+    @IsString()
+    @IsOptional()
+    address?: string
+}
