ac332dfc10d2bf2b1049c28dc9f7abbf9bb1c8b8
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-29 01:22:52 +0530

    feat: implement CRUD operations for CustomersService with error handling

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CustomersService {
-  create(createCustomerDto: CreateCustomerDto) {
-    return 'This action adds a new customer';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createCustomerDto: CreateCustomerDto) {
+    try {
+      return await this.prisma.customer.create({
+        data: createCustomerDto
+      })
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ConflictException('Email already exist');
+      }
+      throw error;
+    }
   }
 
   findAll() {
-    return `This action returns all customers`;
+    return this.prisma.customer.findMany({
+      include: {
+        _count: {
+          select: {
+            sales:true
+          },
+        },
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} customer`;
+  async findOne(id: number) {
+    const customer = await this.prisma.customer.findUnique({
+      where: {id},
+      include: {
+        sales: {
+          orderBy: {createdAt: 'desc'},
+          take: 10,
+        },
+      },
+    });
+
+    if(!customer) {
+      throw new NotFoundException(`Customer with ID ${id} not found`)
+    }
+
+    return customer
   }
 
-  update(id: number, updateCustomerDto: UpdateCustomerDto) {
-    return `This action updates a #${id} customer`;
+  async update(id: number, updateCustomerDto: UpdateCustomerDto) {
+    await this.findOne(id);
+    
+    try {
+
+    } catch (error) {
+      if (error.code == 'P2002') {
+        throw new ConflictException('Email already exist');
+      }
+      throw error;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} customer`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return this.prisma.customer.delete({
+      where: {id}
+    });
   }
 }
