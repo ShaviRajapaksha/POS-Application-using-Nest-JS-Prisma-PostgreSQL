0ee60faafd388007e072aec55cb4a2f7df237880
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-28 23:35:40 +0530

    feat: enhance CategoriesController with API operation summaries and responses

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,33 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
+  @ApiOperation({ summary: 'Create a new category' })
+  @ApiResponse({ status:201, description: 'Category created successfully' })
+  create(@Body(ValidationPipe) createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all categories' })
+  @ApiResponse({ status:200, description: 'Return all categories' } )
   findAll() {
     return this.categoriesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a category by ID' })
+  @ApiResponse({ status: 200, description: 'Return a category' })
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a category' })
+  @ApiResponse({ status:200, description: 'Category updated successfully' })
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoriesService.update(+id, updateCategoryDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a category' })
+  @ApiResponse({ status: 200, description: 'Category is deleted successfully' })
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(+id);
   }
