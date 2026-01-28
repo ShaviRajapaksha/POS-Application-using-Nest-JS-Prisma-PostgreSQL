526a0d737e7b7c4145207513bf2bc420c3d30d8f
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-28 21:49:55 +0530

    feat: add missing modules to AppModule imports for complete application structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CustomersModule } from './customers/customers.module';
+import { SalesModule } from './sales/sales.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule],
+  imports: [ProductsModule, PrismaModule, CategoriesModule, CustomersModule, SalesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
