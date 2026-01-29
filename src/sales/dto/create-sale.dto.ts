6252f53abbc653af45dd975476c11e5b56b714b5
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2026-01-29 16:46:36 +0530

    feat: create CreateSaleDto with validation and API documentation

--- src/sales/dto/create-sale.dto.ts
+++ src/sales/dto/create-sale.dto.ts
@@ -1,1 +1,19 @@
-export class CreateSaleDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsNumber, Min } from "class-validator";
+
+export class CreateSaleDto {
+    @ApiProperty({ example: '1' })
+    @IsInt()
+    productId: number;
+
+    @ApiProperty({ example:2 })
+    @IsInt()
+    @Min(1)
+    quantity: number;
+
+    @ApiProperty({ example: 999.9 })
+    @IsNumber()
+    @Min(0)
+    unitPrice: number;
+
+}
